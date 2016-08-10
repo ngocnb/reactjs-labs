9ec59431a5b1041a2478b5f7d84f3bd884ed5177
Nguyen Bao Ngoc <ngocnguyenbao2707@gmail.com>
2016-08-10 17:38:16 +0700

    add route

--- simple_crud/app/index.jsx
+++ simple_crud/app/index.jsx
@@ -2,3 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+ReactDOM.render(
+  <Provider>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+);
--- simple_crud/app/routes.jsx
+++ simple_crud/app/routes.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './pages/App';
+import PostsIndex from './pages/PostsIndex';
 
 export default(
   <Route path="/" component={App}>
-
+    <IndexRoute component={PostsIndex} />
   </Route>
 );
